788d3321c2f5a754dfc6a63fa05d267a3234d38d
NyanSilence <nyansilence@gmail.com>
2019-09-04 09:43:09 +0700

    remove

--- js/single-slide.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $mod_single_slide = $('.mod-single-slide')
-    $mod_single_slide.find('.slide').slick({
-        arrows: false,
-        dots: false,
-    })
-})
